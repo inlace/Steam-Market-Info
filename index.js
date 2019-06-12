4eb514e0357e76b6b239ee734e4c8d46b919467f
inlace <31384129+inlace@users.noreply.github.com>
2019-06-12 22:48:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        Продаж в сутки
+// @namespace   https://github.com/inlace
+// @version     1.0.0
+// @author      Ilya
+// @description Показывает продажи в сутки
+// @supportURL  https://github.com/inlace
+// @match       http://steamcommunity.com/market/listings/730/*
+// @match       https://steamcommunity.com/market/listings/730/*
+// @license     MIT
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+const link = window.location.href.split("/")[6]
+
+if (link !== null) {
+    getVolume(link)
+        .then(data => addVolume(data.volume));
+}
+
+function getVolume(link) {
+    return fetch(`//steamcommunity.com/market/priceoverview/\
+?appid=730&currency=1&market_hash_name=${link}`)
+        .then(res => res.json());
+}
+
+function addVolume(volume) {
+    const elmDivItem = document.querySelector('#largeiteminfo_item_descriptors');
+    if (elmDivItem !== null) {
+        elmDivItem.innerHTML += `<div class="descriptor">&nbsp;</div>
+<div class="descriptor">Продаж в сутки：<span style="color: #5b9ace">${volume}</span></div>`;
+    }
+}
